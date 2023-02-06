bce69435574c99a4511779eb02f94d915b168466
qismat <qismatnisha6243@gmail.com>
2023-02-06 20:44:39 +0530

    [qismat] Add :UC1-MaximumOfThreeInt

---
+++ GenericsArrProblem/GenericsArrProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericsArrProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] intArray = { 10, 20, 30, };
+            int[] doubleArray = new int[] { 20,60, 60, };
+            Console.WriteLine( doubleArray     );
+        }
+    }
+}
